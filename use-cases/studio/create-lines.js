62c669a696a5a305a3e4253e4c33a6dd996faef7
Flathin <flathinsees@gmail.com>
2024-05-14 16:56:51 -0300

    first commit

---
+++ use-cases/studio/create-lines.js
@@ -0,0 +1,74 @@
+var widthSvg = svg.clientWidth;
+var heightSvg = svg.clientHeight;
+const grid = document.querySelector("#grid");
+
+var spacing = widthSvg / 20;
+
+const lines = [];
+
+function drawGrid() {
+
+  for (let x = spacing; x <= widthSvg; x += spacing) {
+    const l = createLine(x, 0, x, heightSvg);
+
+    grid.appendChild(l);
+  }
+
+  for (let y = spacing; y <= heightSvg; y += spacing) {
+    const l = createLine(0, y, widthSvg, y);
+    grid.appendChild(l);
+  }
+}
+
+function createLine(x1, y1, x2, y2) {
+  const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+
+  line.setAttribute("x1", x1);
+  line.setAttribute("y1", y1);
+  line.setAttribute("x2", x2);
+  line.setAttribute("y2", y2);
+  line.setAttribute("stroke-width", "1");
+  line.setAttribute("stroke", "#94a3b8");
+
+  return line;
+}
+
+function update() {
+  grid.innerHTML = "";
+  widthSvg = svg.viewBox.baseVal.width;
+  heightSvg = svg.viewBox.baseVal.height;
+  var newY = svg.viewBox.baseVal.y;
+  var newX = svg.viewBox.baseVal.x;
+
+  console.log(widthSvg, heightSvg)
+  spacing = widthSvg / 20;
+  
+
+  for (let x = newX; x <= widthSvg; x += spacing) {
+    const l = createLine(x, newY, x, heightSvg);
+    grid.appendChild(l);
+  }
+
+  for (let y = newY; y <= heightSvg; y += spacing) {
+    const l = createLine(newX, y, widthSvg, y);
+    grid.appendChild(l);
+  }
+}
+
+drawGrid();
+
+// Redesenha a grade quando o tamanho da janela é alterado
+// window.addEventListener("resize", function () {
+//   widthSvg = svg.clientWidth;
+//   heightSvg = svg.clientHeight;
+//   drawGrid();
+// });
+
+// Redesenha a grade quando há zoom na página
+// window.addEventListener("wheel", function (event) {
+//   if (event.ctrlKey) {
+//     widthSvg = svg.clientWidth;
+//     heightSvg = svg.clientHeight;
+//     drawGrid();
+//   }
+// });
---
+++ use-cases/studio/use-zoom.js
@@ -0,0 +1,56 @@
+var svgContainer = document.querySelector(".editor");
+var svg = document.getElementById("studio");
+
+var viewBox = svg.viewBox.baseVal;
+var isDragging = false;
+
+var startX, startY;
+var startViewBoxX, startViewBoxY;
+
+
+svgContainer.addEventListener("wheel", function (event) {
+  event.preventDefault();
+
+  var zoomIntensity = 0.1;
+  var delta = event.deltaY > 0 ? -1 : 1;
+
+  var scaleFactor = Math.pow(1 + zoomIntensity, delta);
+  var oldWidth = viewBox.width;
+  var oldHeight = viewBox.height;
+  var mouseX = event.clientX - svg.getBoundingClientRect().left;
+  var mouseY = event.clientY - svg.getBoundingClientRect().top;
+
+  viewBox.width *= scaleFactor;
+  viewBox.height *= scaleFactor;
+  viewBox.x -= (viewBox.width - oldWidth) * (mouseX / svg.width.baseVal.value);
+  viewBox.y -= (viewBox.height - oldHeight) * (mouseY / svg.height.baseVal.value);
+  update();
+});
+
+
+svgContainer.addEventListener("mousedown", function(event) {
+  if(event.button === 0 && event.ctrlKey) {
+    startX = event.layerX;
+    startY = event.layerY;
+    startViewBoxX = viewBox.x;
+    startViewBoxY = viewBox.y;
+    isDragging = true;
+  }
+})
+
+svgContainer.addEventListener("mouseup", function(event) {
+  if (event.button === 0) {
+    isDragging = false;
+  } 
+})
+
+window.addEventListener("mousemove", function (event) {
+  if (isDragging) {
+
+    var deltaX = Math.round(event.layerX - startX);
+    var deltaY = Math.round(event.layerY - startY);
+
+    viewBox.x = startViewBoxX - deltaX;
+    viewBox.y = startViewBoxY - deltaY;
+  }
+});
